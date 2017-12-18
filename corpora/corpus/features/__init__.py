6cca8b53b9a8a1e8a8c1027b5fb3339e788451a4
Douglas Bagnall <douglas@halo.gen.nz>
2017-12-19 11:39:35 +1300

    corpus/features: report language finding failure to stderr

--- corpora/corpus/features/__init__.py
+++ corpora/corpus/features/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, absolute_import
 from importlib import import_module
+import sys
 
 from collections import Counter
 
@@ -12,6 +13,7 @@ def import_finder(lang):
             m = import_module(lang, 'features')
             return m.get_features
         except ImportError as e:
+            print("unknown language %s" % lang, file=sys.stderr)
             return default_feature_finder
         except AttributeError as e:
             return default_feature_finder
