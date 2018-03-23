03bf18ae4abcca3d1d17165a0abe9665715ef020
Keoni Mahelona <keoni@kmahelona.net>
2018-03-24 11:06:13 +1300

    Added another way to calc mahi tahi results

--- corpora/people/views/stats_views.py
+++ corpora/people/views/stats_views.py
@@ -57,7 +57,7 @@ from people.competition import \
     get_invalid_group_members,\
     get_competition_person_score, \
     filter_recordings_for_competition, \
-    mahi_tahi
+    mahi_tahi, mahi_tahi_2
 
 from people.forms import \
     ResendEmailVerificationForm
@@ -330,6 +330,7 @@ class MahiTahi(GroupsStatsView):
 
         for group in groups:
             group.growth_rate = mahi_tahi(group)
+            group.growth_rate_2 = mahi_tahi_2(group)
 
         context['groups'] = groups
         return context
