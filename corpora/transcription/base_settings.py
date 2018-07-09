cb651df9d9b063686d225d5af5f53eace4cd2168
Keoni Mahelona <keoni@kmahelona.net>
2018-07-10 11:40:01 +1200

    Updated DNS for transcribe API as you can't use public dns within ec2s.

--- corpora/transcription/base_settings.py
+++ corpora/transcription/base_settings.py
@@ -3,6 +3,6 @@
 FILE_UPLOAD_PERMISSIONS = 0644
 
 DEEPSPEECH_URL = \
-    "http://LB-corpora-production-deepspeech-1243053555.ap-southeast-2.elb.amazonaws.com/transcribe"
+    "http://deep.koreromaori.io/transcribe"
 
 OLD_URL = "http://waha-tuhi-api-17.dragonfly.nz/transcribe"
