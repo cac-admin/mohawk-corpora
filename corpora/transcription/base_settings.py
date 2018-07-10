19441ff2d8bafb1feda37f2c59013179cd125152
Keoni Mahelona <keoni@kmahelona.net>
2018-07-10 13:58:43 +1200

    Using two load balancers for transribe API. One is for local development and the other is an internal LB for corpora instances.

--- corpora/transcription/base_settings.py
+++ corpora/transcription/base_settings.py
@@ -2,7 +2,15 @@
 
 FILE_UPLOAD_PERMISSIONS = 0644
 
-DEEPSPEECH_URL = \
+DEEPSPEECH_URL_PUBLIC = \
     "http://deep.koreromaori.io/transcribe"
+
+DEEPSPEECH_URL_PRIVATE = \
+    "http://LB-corpora-production-deep-tcp-4113e81f0772e13a.elb.ap-southeast-2.amazonaws.com"
+
+if 'local' in os.environ['ENVIRONMENT_TYPE']:
+    DEEPSPEECH_URL = DEEPSPEECH_URL_PUBLIC
+else:
+    DEEPSPEECH_URL = DEEPSPEECH_URL_PRIVATE
 
 OLD_URL = "http://waha-tuhi-api-17.dragonfly.nz/transcribe"
