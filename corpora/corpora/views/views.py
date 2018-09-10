401a43290e19b6509873d535bddf6e8b918f417b
Keoni Mahelona <keoni@kmahelona.net>
2018-09-10 23:53:22 +1200

    Updated home page.

--- corpora/corpora/views/views.py
+++ corpora/corpora/views/views.py
@@ -27,23 +27,23 @@ from django.views.decorators.cache import cache_page
 
 
 def home(request):
-    if request.user.is_authenticated():
-        return redirect('people:profile')
-    else:
-        groups = Group.objects.all().order_by('name')
-        site = get_current_site(request)
-        context = {
-            'request': request,
-            'groups': groups,
-            'languages': get_unknown_languages(None),
-            'x_title': site.name,
-            'x_description': _("Kōrero Māori is teaching computers indigenous languages.\
-                We've created an open sourced web app to help indigenous\
-                communities strealine their work in language revitalisation."),
-        }
+    # if request.user.is_authenticated():
+    #     return redirect('people:profile')
+    # else:
+    groups = Group.objects.all().order_by('name')
+    site = get_current_site(request)
+    context = {
+        'request': request,
+        'groups': groups,
+        'languages': get_unknown_languages(None),
+        'x_title': site.name,
+        'x_description': _("Kōrero Māori is teaching computers indigenous languages.\
+            We've created an open sourced web app to help indigenous\
+            communities strealine their work in language revitalisation."),
+    }
 
-        return render(request, 'corpora/home.html', context)
-        # return redirect('account/login')
+    return render(request, 'corpora/home.html', context)
+    # return redirect('account/login')
 
 
 def privacy(request):
