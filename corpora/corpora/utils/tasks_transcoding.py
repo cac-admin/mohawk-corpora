a3796c78d0eb671ba8877be56ee2830a4f22a57c
Keoni Mahelona <keoni@kmahelona.net>
2018-12-17 15:23:24 +1300

    comment out tmp file deletion

--- corpora/corpora/utils/tasks_transcoding.py
+++ corpora/corpora/utils/tasks_transcoding.py
@@ -128,8 +128,8 @@ def encode_audio(obj, test=False, codec='aac'):
         return False
 
     # Need a better way to check this!
-    data = commands.getstatusoutput('rm ' + tmp_file)
-    logger.debug('Removed tmp file %s' % (tmp_file))
+    # data = commands.getstatusoutput('rm ' + tmp_file)
+    # logger.debug('Removed tmp file %s' % (tmp_file))
 
     # data = commands.getstatusoutput('rm -r ' + tmp_stor_dir)
     # logger.debug('Removed tmp stor dir %s' % (tmp_stor_dir))
