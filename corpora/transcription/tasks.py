f09a7edad0f70b884ef0a61a9e0798f15bcf4f78
Keoni Mahelona <keoni@kmahelona.net>
2019-04-19 11:53:38 +1200

    Decreased loop limit on wer calc.

--- corpora/transcription/tasks.py
+++ corpora/transcription/tasks.py
@@ -331,5 +331,5 @@ def calculate_wer_for_null():
                 'ERROR calculated wer for Transcription {0}:{1}'.format(
                     t.pk, t.text))
         count = count + 1
-        if count > 1000:
+        if count > 500:
             return "Done"
