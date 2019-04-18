2625f508938bda4f4a972c8420c0231a02b3061f
Keoni Mahelona <keoni@kmahelona.net>
2019-04-19 10:14:22 +1200

    update schedules

--- corpora/transcription/tasks_scheduled.py
+++ corpora/transcription/tasks_scheduled.py
@@ -16,16 +16,16 @@ CELERYBEAT_SCHEDULE = {
     },
     'calc_wer_when_null': {
         'task': 'transcription.tasks.calculate_wer_for_null',
-        'schedule': crontab(minute='*/5', hour='*', day_of_week='*'),
+        'schedule': crontab(minute='*/30', hour='*', day_of_week='*'),
         # 'options': {'task_id': 'xribe_rec_without_rev'},
     },
     'check_for_and_transcribe_blank_segments': {
         'task': 'transcription.tasks.check_and_transcribe_blank_segments',
-        'schedule': crontab(minute='*/10', hour='*', day_of_week='*'),
+        'schedule': crontab(minute='*/35', hour='*', day_of_week='*'),
     },
     'check_for_and_transcribe_blank_audiofiletranscriptions': {
         'task': 'transcription.tasks.check_and_transcribe_blank_audiofiletranscriptions',
-        'schedule': crontab(minute='*/20', hour='*', day_of_week='*'),
+        'schedule': crontab(minute=42, hour=1, day_of_week='*'),
     }
     # 'delete_transcriptions_for_approved_recordings': {
     #     'task': 'transcription.tasks.delete_transcriptions_for_approved_recordings',
