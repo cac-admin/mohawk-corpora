243f479eb88fe8c7428648e866a89ac39d35266a
Keoni Mahelona <keoni@kmahelona.net>
2019-04-20 10:15:35 +1200

    Changed on_delete for content_type field on model as this may be causeing the strange disapearance of objects on migration.

--- corpora/corpus/migrations/0056_auto_20190419_1959.py
+++ corpora/corpus/migrations/0056_auto_20190419_1959.py
@@ -141,7 +141,7 @@ def restore_quality_controls(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('corpus', '0055_auto_20190420_0956'),
+        ('corpus', '0055_auto_20190420_1017'),
     ]
 
     operations = [
