2dda1b08bc77ec76a4184fe5bd034e270f64c39a
Keoni Mahelona <keoni@kmahelona.net>
2019-08-20 18:11:58 +1200

    Fix error in key setting

--- corpora/corpus/views/api.py
+++ corpora/corpus/views/api.py
@@ -329,8 +329,8 @@ class RecordingPermissions(permissions.BasePermission):
             self.message = _("Only staff can read recordings.")
             if request.user.is_staff:
                 person = get_person(request)
-                cache.set('{0}:{0}:listen'.format(person.uuid, obj.id), True, 15)
-                logger.debug('SET KEY '+'{0}:{0}:listen'.format(person.uuid, obj.id))
+                cache.set('{0}:{1}:listen'.format(person.uuid, obj.id), True, 15)
+                logger.debug('SET KEY '+'{0}:{1}:listen'.format(person.uuid, obj.id))
                 return True
             elif person is not None:
                 # Allow people to get their own recordings.
