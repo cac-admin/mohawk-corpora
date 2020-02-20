30ade89b3cf79513e43234716c1aac87e7d99944
Keoni Mahelona <keoni@kmahelona.net>
2020-02-20 15:14:11 +1300

    Added compiled language files... I"m not sure if this is good but there's a chicken and egg problem here that including these will avoid.

--- entry.sh
+++ entry.sh
@@ -9,7 +9,7 @@ mv wahi-korero-0.5.1/wahi_korero/ wahi_korero || true
 rm wahi_korero.tar.gz
 rm -r wahi-korero-0.5.1
 
-python /webapp/corpora/corpora/manage.py compilemessages
+python /webapp/corpora/corpora/manage.py makemessages compilemessages
 python /webapp/corpora/corpora/manage.py migrate --noinput
 python /webapp/corpora/corpora/manage.py bower_install --allow-root
 python /webapp/corpora/corpora/manage.py collectstatic --clear --noinput 
